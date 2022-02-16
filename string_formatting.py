ec6e8c1b11eb1797d2309bc14f4a3431c0d09325
HimanshuDahire <74964402+HimanshuDahire@users.noreply.github.com>
2022-02-16 11:23:32 +0530

    Update string_formatting.py

--- string_formatting.py
+++ string_formatting.py
@@ -19,7 +19,7 @@ def print_formatted(number):
     for i in range(1,n+1):
         print(('{0:d}'.format(i)).rjust(width), end=" ")
         print(('{0:o}'.format(i)).rjust(width), end=" ")
-        print(('{0:x}'.format(i)).upper().rjust(width), end=" ")
+        print(('{0:X}'.format(i)).rjust(width), end=" ")
         print(('{0:b}'.format(i)).rjust(width), end=" ")
         print()
 
